2ca5918779495bb93558be8edb9d9cccc073028f
Jonathan Thompson <jonathanthompson@TL-00309-Jonathon-Thompson.local>
2019-08-08 13:13:08 -0400

    Removal of .idea files

--- loops/squares.py
+++ loops/squares.py
@@ -1,15 +1,15 @@
 square_numbers = []
 
-for value in range(1,11):
+for value in range(1, 11):
     square_number = value**2
     square_numbers.append(square_number)
     
 print(square_numbers)
 
 # List comprehension format for the above listing
-comprehension_square_numbers = [value**2 for value in range(1,11)]
+comprehension_square_numbers = [value**2 for value in range(1, 11)]
 print(comprehension_square_numbers)
 
 # Same idea but cubed instead of squared
-cubed_numbers = [value**3 for value in range(1,11)]
+cubed_numbers = [value**3 for value in range(1, 11)]
 print(cubed_numbers)
