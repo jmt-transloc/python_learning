9742b2f66a969e183fe87e5cde1158f4455ede1c
Jonathan Thompson <jonathanthompson@TL-00309-Jonathon-Thompson.local>
2019-09-24 16:37:11 -0400

    Config refactoring

--- testing/automation/pytest/tests/test_web.py
+++ testing/automation/pytest/tests/test_web.py
@@ -13,20 +13,31 @@ def config():
         data = json.load(config_file)
     return data
 
+@pytest.fixture(scope='session')
+def config_browser(config):
+    if 'browser' not in config:
+        raise Exception('The configuration file does not contain "browser"')
+    elif config['browser'] not in ['chrome']:
+        raise Exception(f'"{config["browser"]}" is not a supported browser')
+    return config['browser']
+
+@pytest.fixture(scope='session')
+def config_wait_time(config):
+    return config['wait_time'] if 'wait_time' in config else 10
 #
 # Create a browser fixture. Fixtures are called once per test function
 # unless otherwise specified.
 #
 @pytest.fixture
-def browser(config):
-    if config['browser'] == 'chrome':
+def browser(config_browser, config_wait_time):
+    if config_browser == 'chrome':
         driver = Chrome()
     else:
         raise Exception(
             f'"{config["browser"]}" is not a supported browser'
         )
 
-    driver.implicitly_wait(config['wait_time'])
+    driver.implicitly_wait(config_wait_time)
     yield driver
     driver.quit()
 
